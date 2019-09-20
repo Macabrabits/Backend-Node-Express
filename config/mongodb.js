0d7dd27023216abb7d43d553560bf510a7c75a18
Victor Mattar <suporte@trustit.com.br>
2019-09-20 14:48:09 -0300

    implementação de mongodb e api de stats

---
+++ config/mongodb.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true })
+    .then(console.log('connected to mongodb!!'))
+    .catch(e => {
+        const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
+        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
+    })
+
+
